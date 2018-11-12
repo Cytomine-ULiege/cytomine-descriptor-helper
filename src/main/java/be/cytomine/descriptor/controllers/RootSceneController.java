5d08c5deee6494b4fa8416c2ba0e2c21396f2e14
Waliens <romainmormont@hotmail.com>
2018-11-12 15:59:53 +0100

    almost full forms

--- src/main/java/be/cytomine/descriptor/controllers/RootSceneController.java
+++ src/main/java/be/cytomine/descriptor/controllers/RootSceneController.java
@@ -1,10 +1,10 @@
 package be.cytomine.descriptor.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -30,6 +30,23 @@ public class RootSceneController implements Initializable {
     @FXML public TextField cliPythonField;
     @FXML public Label cliScriptLabel;
     @FXML public TextField cliScriptField;
+    @FXML public Label editParamsTitleLabel;
+    @FXML public Label paramIdLabel;
+    @FXML public TextField paramIdField;
+    @FXML public Label paramTypeLabel;
+    @FXML public ComboBox<String> paramNameCombo;
+    @FXML public Label paramDefaultLabel;
+    @FXML public TextField paramDefaultField;
+    @FXML public Label optionalLabel;
+    @FXML public CheckBox optionalCheckBox;
+    @FXML public Label setByServerLabel;
+    @FXML public CheckBox setByServerCheckBox;
+    @FXML public Label paramNameLabel;
+    @FXML public TextField paramNameField;
+    @FXML public Label paramDescLabel;
+    @FXML public TextArea paramDescField;
+
+    private ObservableList<String> types;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -47,6 +64,19 @@ public class RootSceneController implements Initializable {
         cliPythonField.setText("python");
         cliScriptLabel.setText("Script");
         cliScriptField.setText("wrapper.py");
+        editParamsTitleLabel.setText("Parameters");
+        paramIdLabel.setText("Id");
+        paramNameLabel.setText("Name");
+        paramDescLabel.setText("Description");
+        paramTypeLabel.setText("Type");
+        paramDefaultLabel.setText("Default value");
+        optionalLabel.setText("Optional");
+        setByServerLabel.setText("Set-by-server");
+
+        types = FXCollections.observableArrayList();
+        types.addAll("Number", "String", "Boolean", "Domain", "ListDomain", "Date");
+        paramNameCombo.setItems(types);
+
 
     }
 }
